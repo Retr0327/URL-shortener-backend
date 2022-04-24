c0d0c2608451a20fcff5ba45ffce1bb372ba347e
Retr0327 <philcoke35@gmail.com>
2022-04-24 23:53:58 +0800

    Add DockerEnvType

---
+++ src/typings.d.ts
@@ -0,0 +1,11 @@
+export type DockerEnvType = {
+  pgUser: string;
+  pgHost: string;
+  pgDatabase: string;
+  pgPassword: string;
+  pgPort: number;
+  redisHost: string;
+  redisPort: number;
+  port: number;
+  period: string;
+};
