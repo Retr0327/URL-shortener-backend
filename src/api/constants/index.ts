61b898d8c7d36ed46a46ac8c7b7bf7baf3ccfc32
Retr0327 <philcoke35@gmail.com>
2022-04-24 23:44:58 +0800

    Update dockerEnv

--- src/api/constants/index.ts
+++ src/api/constants/index.ts
@@ -1,4 +1,11 @@
 const dockerEnv = {
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
   port: process.env.PORT,
   period: process.env.NODE_ENV,
 };
