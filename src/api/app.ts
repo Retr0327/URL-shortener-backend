574b3c7466fbd8c54e2f85a51fde5e2dd32a241c
Retr0327 <philcoke35@gmail.com>
2022-04-24 23:19:28 +0800

    Set urlencoded true

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,10 +1,12 @@
 import Koa from "koa";
 import cors from "@koa/cors";
+import koaBody from "koa-body";
 import corsConfig from "./helpers/corsConfig";
 import { rootRoute, urlRoute } from "./routes";
 
 const app = new Koa();
 app.use(cors(corsConfig));
+app.use(koaBody({ urlencoded: true }));
 
 app.use(rootRoute.routes());
 app.use(rootRoute.allowedMethods());
