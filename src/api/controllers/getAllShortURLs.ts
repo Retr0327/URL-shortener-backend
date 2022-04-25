163d24e7298f81e79389a49453a7a384ffc381af
Retr0327 <philcoke35@gmail.com>
2022-04-25 19:55:32 +0800

    Update handleGetAllShortURLs

--- src/api/controllers/getAllShortURLs.ts
+++ src/api/controllers/getAllShortURLs.ts
@@ -1,5 +1,10 @@
 import { Context } from "koa";
+import { getAllShortURLs } from "../services/shortURLServices";
 
-const handleGetAllShortURLs = async (ctx: Context) => {};
+const handleGetAllShortURLs = async (ctx: Context) => {
+  const result = await getAllShortURLs();
+  ctx.status = 200;
+  ctx.body = { status: "success", data: result };
+};
 
 export default handleGetAllShortURLs;
