f80b54b3596e00fb92e84f306e7d9d57b69359c6
Retr0327 <philcoke35@gmail.com>
2022-04-25 09:55:07 +0800

    Add url sql

---
+++ src/api/models/sql/url.sql
@@ -0,0 +1,12 @@
+SET TIME ZONE 'UTC';
+
+BEGIN;
+
+CREATE TABLE shortened_urls (
+    full_url TEXT NOT NULL,
+    short_url TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expire TIMESTAMP NOT NULL
+);
+
+COMMIT;
