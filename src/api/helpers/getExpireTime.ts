0209820a4eeb335af225c94dc476e451390b2284
Retr0327 <philcoke35@gmail.com>
2022-04-25 22:37:27 +0800

    Add getExpireTime

---
+++ src/api/helpers/getExpireTime.ts
@@ -0,0 +1,11 @@
+function getExpireTime(time: string) {
+  const seconds = (new Date(time).getTime() - Date.now()) / 1000;
+  const aDay = 86400;
+
+  if (seconds > aDay) {
+    return null;
+  }
+  return seconds;
+}
+
+export default getExpireTime;
--- src/api/helpers/index.ts
+++ src/api/helpers/index.ts
@@ -1,5 +1,6 @@
 import cacheURLs from "./cacheURLs";
 import corsConfig from "./corsConfig";
 import generateShortUrl from "./generateShortURL";
+import getExpireTime from "./getExpireTime";
 
-export { cacheURLs, corsConfig, generateShortUrl };
+export { cacheURLs, corsConfig, generateShortUrl, getExpireTime };
