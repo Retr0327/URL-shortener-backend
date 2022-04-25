238c6303a9d34c531fbb0ff203748213700d5a84
Retr0327 <philcoke35@gmail.com>
2022-04-25 10:27:32 +0800

    Update validateURL

--- src/api/validations/urlValidations.ts
+++ src/api/validations/urlValidations.ts
@@ -1,17 +1,13 @@
-import { URL } from "url";
 import { Context, Next } from "koa";
-
-function isURL(url: string) {
-  try {
-    new URL(url);
-    return true;
-  } catch (err) {
-    return false;
-  }
-}
+import { isURL } from "./validationHelper";
 
 const validateURL = async (ctx: Context, next: Next) => {
   const { url, expireDate } = ctx.request.body;
+  let errorCount: number = 0;
+
+  if (!isURL(url)) {
+    errorCount++;
+  }
 
   next();
 };
