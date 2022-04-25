aafaad6e237757c282836ea24b39b9fdbe914841
Retr0327 <philcoke35@gmail.com>
2022-04-25 13:50:00 +0800

    Add shortURLServices

---
+++ src/api/services/shortURLServices.ts
@@ -0,0 +1,33 @@
+import { pgCli } from "../models";
+
+async function createShortUrlByUrl(
+  url: string,
+  shortUrl: string,
+  expire: string
+) {
+  const { rows } = await pgCli.query(
+    `INSERT INTO shortened_urls (
+        full_url, 
+        short_url, 
+        expire)
+     VALUES ($1, $2, $3)
+     RETURNING id, short_url
+    `,
+    [url, shortUrl, expire]
+  );
+
+  return rows;
+}
+
+async function deleteShortURLByShortURL(shortURL: string) {
+  const { rows } = await pgCli.query(
+    `DELETE FROM shortened_urls 
+     WHERE short_url = $1;
+    `,
+    [shortURL]
+  );
+
+  return rows;
+}
+
+export { createShortUrlByUrl, deleteShortURLByShortURL };
