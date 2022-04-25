d6ae0f14f935a2fe0dcc6bfe3aaf82bd5c1b81ae
Retr0327 <philcoke35@gmail.com>
2022-04-25 21:52:34 +0800

    Add index

---
+++ src/api/helpers/index.ts
@@ -0,0 +1,5 @@
+import cacheURLs from "./cacheURLs";
+import corsConfig from "./corsConfig";
+import generateShortUrl from "./generateShortURL";
+
+export { cacheURLs, corsConfig, generateShortUrl };
