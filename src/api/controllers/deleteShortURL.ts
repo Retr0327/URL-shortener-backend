ea3187bec6c8640067de6429d3dea89e95c955f4
Retr0327 <philcoke35@gmail.com>
2022-04-26 15:12:02 +0800

    Update handleDeleteShortURL

--- src/api/controllers/deleteShortURL.ts
+++ src/api/controllers/deleteShortURL.ts
@@ -1,7 +1,17 @@
 import { Context } from "koa";
+import { deleteCachedURLs } from "../helpers/redis";
+import { deleteShortUrlByShortURL } from "../services/shortURLServices";
 
 const handleDeleteShortURL = async (ctx: Context) => {
-  ctx.status = 204;
+  const { shortURL } = ctx.request.body;
+
+  await Promise.all([
+    deleteShortUrlByShortURL(shortURL),
+    deleteCachedURLs(shortURL),
+  ]);
+
+  ctx.status = 202;
+  ctx.body = { status: "success" };
 };
 
 export default handleDeleteShortURL;
