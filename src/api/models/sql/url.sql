3f197e1e31f99a044d5e17b4ee6974c3645bcf1d
Retr0327 <philcoke35@gmail.com>
2022-04-26 20:59:12 +0800

    Add `total_click`

--- src/api/models/sql/url.sql
+++ src/api/models/sql/url.sql
@@ -6,6 +6,7 @@ CREATE TABLE shortened_urls (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     full_url TEXT NOT NULL,
     short_url TEXT NOT NULL,
+    total_click INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     expire TIMESTAMP NOT NULL
 );
