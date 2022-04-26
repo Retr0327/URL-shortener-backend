9a3afa6e3d8a7e458c2e451f61e987c9b790214a
Retr0327 <philcoke35@gmail.com>
2022-04-26 19:45:05 +0800

    Update handleRedirectURL

--- src/api/controllers/redirectURL.ts
+++ src/api/controllers/redirectURL.ts
@@ -1,7 +1,20 @@
 import { Context } from "koa";
+import { ShortURLResult } from "src/typings";
+import { getFullURLByShortURL } from "../services/shortURLServices";
 
 const handleRedirectURL = async (ctx: Context) => {
-  ctx.status = 200;
+  const { shortURL } = ctx.params;
+  const result = await getFullURLByShortURL(shortURL);
+
+  if (!result!.length) {
+    ctx.status = 404;
+    return;
+  }
+
+  const { full_url: fullURL }: ShortURLResult = result![0];
+
+  ctx.status = 302;
+  ctx.redirect(fullURL);
 };
 
 export default handleRedirectURL;
