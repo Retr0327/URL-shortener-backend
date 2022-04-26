01ba7af4759af1e2ce2887ba3b3f8cb2c76209eb
Retr0327 <philcoke35@gmail.com>
2022-04-26 20:57:37 +0800

    Add increase route

--- src/api/routes/url.ts
+++ src/api/routes/url.ts
@@ -3,6 +3,7 @@ import {
   handleCreateShortURL,
   handleDeleteShortURL,
   handleGetAllShortURLs,
+  handleIncreaseClick,
 } from "../controllers";
 import { checkLongURLExists } from "../middlewares";
 import validateURL from "../validations/urlValidations";
@@ -11,6 +12,7 @@ const router = new KoaRouter({ prefix: "/url" });
 
 router.post("/", validateURL, checkLongURLExists, handleCreateShortURL);
 router.post("/all", handleGetAllShortURLs);
+router.put("/increase", handleIncreaseClick);
 router.post("/delete", handleDeleteShortURL);
 
 export { router as urlRoute };
