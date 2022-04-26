6b3416cc75c5bd699bdc05833af99c58718e8bf8
Retr0327 <philcoke35@gmail.com>
2022-04-26 21:06:08 +0800

    Update handleIncreaseClick

--- src/api/controllers/increaseClick.ts
+++ src/api/controllers/increaseClick.ts
@@ -1,9 +1,18 @@
 import { Context } from "koa";
+import { updateTotalClickByShortURL } from "../services/shortURLServices";
 
 const handleIncreaseClick = async (ctx: Context) => {
-  console.log(ctx.request.body);
+  const { shortURL } = ctx.request.body;
+  const result = await updateTotalClickByShortURL(shortURL);
 
-  ctx.status = 200;
+  if (!result?.length) {
+    ctx.status = 500;
+    ctx.body = { status: "failed", message: "Cannot add" };
+    return;
+  }
+
+  ctx.status = 201;
+  ctx.body = { status: "success" };
 };
 
 export default handleIncreaseClick;
