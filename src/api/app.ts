87cd99cce5f03b4f5d49cba14e0ac7c73c61e2d2
Retr0327 <philcoke35@gmail.com>
2022-04-26 20:36:37 +0800

    Add helmet

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,11 +1,14 @@
 import Koa from "koa";
 import cors from "@koa/cors";
 import koaBody from "koa-body";
+import helmet from "koa-helmet";
 import { corsConfig } from "./helpers";
 import { rootRoute, urlRoute, redirectRoute } from "./routes";
 
 const app = new Koa();
+
 app.use(cors(corsConfig));
+app.use(helmet());
 app.use(koaBody({ urlencoded: true }));
 
 app.use(rootRoute.routes());
