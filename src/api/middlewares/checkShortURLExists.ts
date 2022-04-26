e09faab98fc27eac06ad9368464ab5275ba73740
Retr0327 <philcoke35@gmail.com>
2022-04-26 20:16:07 +0800

    Update checkShortURLExists

--- src/api/middlewares/checkShortURLExists.ts
+++ src/api/middlewares/checkShortURLExists.ts
@@ -4,8 +4,13 @@ import { getCachedURL } from "../helpers/redis";
 const checkShortURLExists = async (ctx: Context, next: Next) => {
   const { shortURL } = ctx.params;
   const result = await getCachedURL(shortURL);
-  console.log(result)
-  return next();
+
+  if (!Object.keys(result).length) {
+    return next();
+  }
+
+  ctx.status = 302;
+  ctx.redirect(result.url);
 };
 
 export default checkShortURLExists;
