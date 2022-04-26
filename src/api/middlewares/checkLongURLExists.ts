ab835ce542876b93cb81c70af79f48e7c5fab85f
Retr0327 <philcoke35@gmail.com>
2022-04-26 20:29:03 +0800

    Update checkLongURLExists

--- src/api/middlewares/checkLongURLExists.ts
+++ src/api/middlewares/checkLongURLExists.ts
@@ -1,15 +1,18 @@
 import { Context, Next } from "koa";
+import { ShortURLResult, CreatedURLRequestBody } from "types";
 import { getShortURLByFullURL } from "../services/shortURLServices";
 
 const checkLongURLExists = async (ctx: Context, next: Next) => {
-  const { url, expireDate } = ctx.request.body;
+  const { url }: CreatedURLRequestBody = ctx.request.body;
   const result = await getShortURLByFullURL(url);
 
-console.log(result)
+  if (!result?.length) {
+    return next();
+  }
+  const { id, short_url: shortURL }: ShortURLResult = result[0];
 
-
-//   return next()
-
+  ctx.status = 200;
+  ctx.body = { status: "success", id, shortURL };
 };
 
 export default checkLongURLExists;
