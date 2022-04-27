cb9feaec03f65bd9104c018c74ea9eb02d22f2c8
Retr0327 <philcoke35@gmail.com>
2022-04-27 20:13:11 +0800

    Add production url

--- src/api/helpers/corsConfig.ts
+++ src/api/helpers/corsConfig.ts
@@ -1,16 +1,23 @@
 import { Options } from "@koa/cors";
+import { dockerEnv } from "../constants";
+
+const { period } = dockerEnv;
 
 const corsConfig: Options = {
   origin: (ctx) => {
     if (ctx.url === "/") {
       return "*";
     }
+    let url = "http://localhost:3001";
 
-    return "http://localhost:3001";
+    if (period === "production") {
+      url = "http://client";
+    }
+
+    return url;
   },
   credentials: true,
   allowMethods: ["GET", "POST", "PUT"],
 };
 
 export default corsConfig;
-    
