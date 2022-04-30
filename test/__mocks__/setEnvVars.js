f5f013717cd479ac04e2b222421821a2ec7df15e
Retr0327 <philcoke35@gmail.com>
2022-04-30 10:45:34 +0800

    Add postgreSQL password

--- test/__mocks__/setEnvVars.js
+++ test/__mocks__/setEnvVars.js
@@ -1,5 +1,6 @@
 process.env.PGUSER = "postgres";
 process.env.PGHOST = "postgres";
+process.env.PGPASSWORD = "postgres";
 process.env.PGDATABASE = "url-shortener";
 process.env.PGPORT = "5432";
 process.env.PORRT = "3000";
