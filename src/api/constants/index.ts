32e104f63cafed5732467cb7ce4d80e8b37976e3
Retr0327 <philcoke35@gmail.com>
2022-05-01 15:35:19 +0800

    Remove dockerEnv

--- src/api/constants/index.ts
+++ src/api/constants/index.ts
@@ -1,16 +1,4 @@
 const shortURLTable = "shortened_urls";
 const redisKey = (shortURL: string) => `shortURL:${shortURL}`;
 
-const dockerEnv = {
-  pgUser: process.env.PGUSER,
-  pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
-  pgPort: process.env.PGPORT,
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
-  port: process.env.PORT,
-  period: process.env.NODE_ENV,
-};
-
-export { dockerEnv, shortURLTable, redisKey };
+export { shortURLTable, redisKey };
