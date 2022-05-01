de1f407585eb3dcb4b1c328a2cabd9a321c79dd9
Retr0327 <philcoke35@gmail.com>
2022-05-01 17:51:32 +0800

    Test production models

--- test/models.test/models.test.ts
+++ test/models.test/models.test.ts
@@ -2,7 +2,19 @@ import { pgConfig } from "src/api/models/pgCli";
 import { redisConfig } from "src/api/models/redisCli";
 
 describe("Test models config", () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...OLD_ENV };
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV;
+  });
+
   test("should return postgres config in production", () => {
+    process.env.NODE_ENV = "production";
     const config = pgConfig();
     expect(config).toHaveProperty("host");
     expect(config).toHaveProperty("user");
@@ -12,6 +24,7 @@ describe("Test models config", () => {
   });
 
   test("should return redis config in production", () => {
+    process.env.NODE_ENV = "production";
     const config = redisConfig();
     expect(config).not.toBe("");
   });
