e4c0c90fc226d933b70bbda3df07e4586488e73d
Retr0327 <philcoke35@gmail.com>
2022-05-02 15:38:37 +0800

    Update endpoint test

--- test/e2e/endpoint.test.ts
+++ test/e2e/endpoint.test.ts
@@ -1,6 +1,7 @@
 import server from "src/server";
 import request from "supertest";
 import { deleteCachedURL } from "src/api/helpers/redis";
+import { getShortURLByFullURL } from "src/api/services/shortURLServices";
 
 let expireDate: Date;
 
@@ -13,7 +14,7 @@ afterAll(() => {
 });
 
 describe("POST Endpoints", () => {
-  describe("Post /api/urls", () => {
+  describe("Post /api/url", () => {
     test("should return id and short url", async () => {
       const response = await request(server).post("/url").send({
         url: "https://github.com/Retr0327",
@@ -24,12 +25,6 @@ describe("POST Endpoints", () => {
       expect(response.body.status).toBe("success");
       expect(response.body).toHaveProperty("id");
       expect(response.body).toHaveProperty("shortURL");
-
-      const deleteResponse = await request(server).post("/url/delete").send({
-        shortURL: response.body.shortUrl,
-      });
-
-      expect(deleteResponse.statusCode).toEqual(202);
     });
 
     test(`should return error {"error": [{"message": "Invalid URL"}]}`, async () => {
@@ -67,5 +62,49 @@ describe("POST Endpoints", () => {
       expect(response.statusCode).toEqual(400);
       expect(response.body).toMatchObject(actual);
     });
+  });
+
+  describe("Post /api/url/all", () => {
+    test("should return array of objects", async () => {
+      const response = await request(server).post("/url/all");
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.body.status).toBe("success");
+      expect(response.body.data).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
+    });
+  });
+});
+
+describe("GET Endpoints", () => {
+  describe("Get /api/:shortURL", () => {
+    test("should return fullURL", async () => {
+      const result = await getShortURLByFullURL("https://github.com/Retr0327");
+      const { short_url: shortURL } = result![0];
+
+      const actual = {
+        status: "success",
+        fullURL: "https://github.com/Retr0327",
+      };
+
+      const response = await request(server).get(`/${shortURL}`);
+      expect(response.body).toMatchObject(actual);
+    });
+  });
+});
+
+describe("POST /api/url/delete", () => {
+  test("should return success", async () => {
+    const result = await getShortURLByFullURL("https://github.com/Retr0327");
+
+    const { short_url: shortURL } = result![0];
+
+    const response = await request(server).post("/url/delete").send({
+      shortURL,
+    });
+
+    expect(response.statusCode).toEqual(202);
+    expect(response.body.status).toBe("success");
   });
 });
