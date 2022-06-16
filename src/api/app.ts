72f977f165392fca35f523e402f011052cd4f2c6
Retr0327 <philcoke35@gmail.com>
2022-06-16 23:25:55 +0800

    Add customDevFormat

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,15 +1,20 @@
 import Koa from "koa";
 import cors from "@koa/cors";
 import helmet from "koa-helmet";
+import morgan from "koa-morgan";
 import { corsConfig } from "./helpers";
 import bodyParser from "koa-bodyparser";
+import { customDevFormat } from "@utils";
 import { rootRoute, urlRoute, redirectRoute } from "./routes";
+
+morgan.format("custom-dev", customDevFormat);
 
 const app = new Koa();
 
 app.use(cors(corsConfig));
 app.use(helmet());
 app.use(bodyParser());
+app.use(morgan("custom-dev"));
 
 app.use(rootRoute.routes());
 app.use(rootRoute.allowedMethods());
