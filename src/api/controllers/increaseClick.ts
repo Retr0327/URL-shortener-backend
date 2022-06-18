471f781b7d6c9a18a7c01976d61f6d91edbfef02
Retr0327 <philcoke35@gmail.com>
2022-06-19 02:20:20 +0800

    Update handleIncreaseClick

--- src/api/controllers/increaseClick.ts
+++ src/api/controllers/increaseClick.ts
@@ -1,21 +1,25 @@
-import { Context } from "koa";
-import { ShortURLResult } from "src/typings";
-import { updateTotalClickByShortURL } from "../services/shortURLServices";
+import { RouterContext } from "@koa/router";
+import { PrismaClient } from "@prisma/client";
 
-const handleIncreaseClick = async (ctx: Context) => {
+const { shortUrls } = new PrismaClient();
+
+const handleIncreaseClick = async (ctx: RouterContext) => {
   const { shortURL } = ctx.request.body;
-  const result = await updateTotalClickByShortURL(shortURL);
 
-  if (!result?.length) {
+  const updateResult = await shortUrls.update({
+    where: { shortURL },
+    data: { totalClick: { increment: 1 } },
+    select: { fullURL: true },
+  });
+
+  if (updateResult == null) {
     ctx.status = 500;
     ctx.body = { status: "failed", message: "Cannot add" };
     return;
   }
 
-  const { full_url }: ShortURLResult = result[0];
-
   ctx.status = 201;
-  ctx.body = { status: "success", url: full_url };
+  ctx.body = { status: "success", url: updateResult.fullURL };
 };
 
 export default handleIncreaseClick;
