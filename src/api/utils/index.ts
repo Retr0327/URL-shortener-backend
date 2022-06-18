6bd117b4c8346a76d1b06204b2ac2e388a85bafc
Retr0327 <philcoke35@gmail.com>
2022-06-18 16:48:59 +0800

    Add cacheURL

--- src/api/utils/index.ts
+++ src/api/utils/index.ts
@@ -2,5 +2,12 @@ import customDevFormat from "./logger";
 import getExpireTime from "./expireTime";
 import generateRedisKey from "./redisKey";
 import generateShortURL from "./shortURLGenerator";
+import { cacheURL } from "./cache";
 
-export { getExpireTime, customDevFormat, generateShortURL, generateRedisKey };
+export {
+  getExpireTime,
+  customDevFormat,
+  generateShortURL,
+  generateRedisKey,
+  cacheURL,
+};
