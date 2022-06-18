b64b6a50ecd69576d867a4939acf2a0c4cc13d65
Retr0327 <philcoke35@gmail.com>
2022-06-18 16:43:17 +0800

    Add expireTime

--- src/api/utils/index.ts
+++ src/api/utils/index.ts
@@ -1,5 +1,5 @@
 import customDevFormat from "./logger";
-import getExpireTime from "./getExpireTime";
+import getExpireTime from "./expireTime";
 import generateShortURL from "./shortURLGenerator";
 
 export { getExpireTime, customDevFormat, generateShortURL };
