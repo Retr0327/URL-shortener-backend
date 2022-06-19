3e0e65d442a4de55c3c861d316a9fc700485d8f1
Retr0327 <philcoke35@gmail.com>
2022-06-19 23:00:36 +0800

    Update ip format

--- test/e2e/root.test.ts
+++ test/e2e/root.test.ts
@@ -8,7 +8,7 @@ afterAll(() => {
 describe("Test root /", () => {
   test("should return success and ip", async () => {
     const response = await request(server).get("/");
-    const actual = { status: "success", ip: "::ffff:127.0.0.1" };
+    const actual = { status: "success", ip: "127.0.0.1" };
 
     expect(response.statusCode).toEqual(200);
     expect(response.body).toMatchObject(actual);
