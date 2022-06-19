8fb308a8422a42c8fc9411e5950ac1c2d11b2964
Retr0327 <philcoke35@gmail.com>
2022-06-20 01:28:40 +0800

    Add checkShortURLExists

---
+++ src/api/middlewares/urlChecker/index.ts
@@ -0,0 +1,5 @@
+import checkLongURLExists from "./longURLChecker";
+import checkShortURLTTL from "./shortURLTTLChecker";
+import checkShortURLExists from "./shortURLChecker";
+
+export { checkLongURLExists, checkShortURLTTL, checkShortURLExists };
