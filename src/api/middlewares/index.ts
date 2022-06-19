a0c7fdf457af75c7907e721c6b14b3d606481451
Retr0327 <philcoke35@gmail.com>
2022-06-20 01:28:53 +0800

    Remove unecessary imports

--- src/api/middlewares/index.ts
+++ src/api/middlewares/index.ts
@@ -1,11 +1,3 @@
 import validate from "./validation";
-import checkLongURLExists from "./longURLChecker";
-import checkShortURLTTL from "./shortURLTTLChecker";
-import checkShortURLExists from "./shortURLChecker";
 
-export {
-  validate,
-  checkShortURLExists,
-  checkLongURLExists,
-  checkShortURLTTL,
-};
+export { validate };
