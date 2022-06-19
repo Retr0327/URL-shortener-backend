29cf4df584c42743a0d2ccd0532791a82921ee49
Retr0327 <philcoke35@gmail.com>
2022-06-19 11:43:59 +0800

    Remove CreatedURLRequestBody

--- src/api/types/index.ts
+++ src/api/types/index.ts
@@ -1,14 +1,6 @@
 import { IncomingMessage } from "http";
 import { Session } from "koa-generic-session";
 
-export type ShortURLResult = {
-  id: string;
-  full_url: string;
-  short_url: string;
-  created_at: Date;
-  expire: Date;
-};
-
 export type CreatedURLRequestBody = {
   url: string;
   expireDate: string;
