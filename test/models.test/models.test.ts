418aa1c9fe65b3d7238f37581718ce6db4db3e17
Retr0327 <philcoke35@gmail.com>
2022-06-20 22:39:07 +0800

    Update models test

--- test/models.test/models.test.ts
+++ test/models.test/models.test.ts
@@ -1,7 +1,6 @@
-import { pgConfig } from "src/api/models/pgCli";
 import { redisConfig } from "src/api/models/redisCli";
 
-describe("Test models config", () => {
+describe("Test Redis config", () => {
   const OLD_ENV = process.env;
 
   beforeEach(() => {
@@ -13,19 +12,10 @@ describe("Test models config", () => {
     process.env = OLD_ENV;
   });
 
-  test("should return postgres config in production", () => {
-    process.env.NODE_ENV = "production";
-    const config = pgConfig();
-    expect(config).toHaveProperty("host");
-    expect(config).toHaveProperty("user");
-    expect(config).toHaveProperty("database");
-    expect(config).toHaveProperty("password");
-    expect(config).toHaveProperty("port");
-  });
-
   test("should return redis config in production", () => {
     process.env.NODE_ENV = "production";
     const config = redisConfig();
+    expect(config).toBe("redis://redis:6379");
     expect(config).not.toBe("");
   });
 });
