eb0fd066e46fe0c1f7cd00476c88e40d8bd7cade
Retr0327 <philcoke35@gmail.com>
2022-10-10 23:44:19 +0800

    Load environment variables

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import app from "./api/app";
-
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, () =>
-  console.log(`⚡️[server] Listening on port ${PORT}`)
-);
-
-export default server;
