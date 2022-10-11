d246fcd0213c68ea9b75613279a792a9bdaf4691
Retr0327 <philcoke35@gmail.com>
2022-10-11 15:02:25 +0800

    Add handleRedirectURL

---
+++ src/api/controllers/redirect/index.ts
@@ -0,0 +1,33 @@
+import { Middleware } from 'koa';
+import { prisma } from '@models';
+
+const handleRedirectURL: Middleware = async (ctx) => {
+  const { shortURL } = ctx.params;
+  const longURLResult = await prisma.shortUrls.findUnique({
+    where: { shortURL },
+    select: { fullURL: true },
+  });
+
+  if (longURLResult === null) {
+    ctx.status = 200;
+    ctx.body = { status: 'success', msg: 'expired' };
+    return;
+  }
+
+  const updateResult = await prisma.shortUrls.update({
+    where: { shortURL },
+    data: { totalClick: { increment: 1 } },
+    select: { fullURL: true },
+  });
+
+  if (updateResult === null) {
+    ctx.status = 500;
+    ctx.body = { status: 'failed', msg: 'internal server error' };
+    return;
+  }
+
+  ctx.status = 200;
+  ctx.body = { status: 'success', data: { fullURL: updateResult.fullURL } };
+};
+
+export default handleRedirectURL;
