47f47d64e04959206203a8fcc10d62e5d8302315
Retr0327 <philcoke35@gmail.com>
2022-10-11 14:28:25 +0800

    Remove urlRoutes

--- src/api/routes/redirectURLRoute.ts
+++
@@ -1,9 +0,0 @@
-import Router from "@koa/router";
-import { handleRedirectURL } from "@controllers";
-import { checkShortURLExists } from "@middlewares/urlChecker";
-
-const router = new Router({});
-
-router.get("/:shortURL", checkShortURLExists, handleRedirectURL);
-
-export { router as redirectURLRoute };
