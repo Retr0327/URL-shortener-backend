35a7367d0d2f2238af08f5db33356fc6a2711691
Retr0327 <philcoke35@gmail.com>
2022-10-14 00:42:02 +0800

    Test redis functionalities

--- test/utils.test/redis.test.ts
+++ test/utils.test/redis.test.ts
@@ -1,5 +1,5 @@
-const Redis = require("ioredis-mock");
-import { cacheURL, removeCachedURL, getCachedURL } from "@utils";
+import Redis from 'ioredis-mock';
+import { hsetURL, getCachedURL, removeCachedURL } from '@utils/redis';
 
 let redis: any;
 let id: string;
@@ -9,31 +9,31 @@ let expire: number;
 
 beforeAll(() => {
   redis = new Redis();
-  id = "1";
-  longURL = "https://www.google.com";
-  shortURL = "abcde";
+  id = '1';
+  longURL = 'https://www.google.com';
+  shortURL = 'abcde';
   expire = 3600;
 });
 
-jest.setMock("ioredis", redis);
+jest.setMock('ioredis', redis);
 
 afterEach((done) => {
   new Redis().flushall().then(() => done());
 });
 
-describe("Test utils/cache", () => {
-  describe("Test utils/cacheURL", () => {
+describe('Test utils/cache', () => {
+  describe('Test utils/cacheURL', () => {
     test("should return { id: '1', url: 'https://www.google.com' }", async () => {
-      await cacheURL(id, longURL, shortURL, expire);
+      await hsetURL(id, longURL, shortURL, expire);
       const result = await getCachedURL(shortURL);
-      const actual = { id: "1", url: "https://www.google.com" };
+      const actual = { id: '1', url: 'https://www.google.com' };
 
       expect(result).toMatchObject(actual);
     });
   });
 
-  describe("Test utils/removeCachedURL", () => {
-    test("should return {}", async () => {
+  describe('Test utils/removeCachedURL', () => {
+    test('should return {}', async () => {
       await removeCachedURL(shortURL);
       const result = await getCachedURL(shortURL);
 
