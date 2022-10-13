5762f51a7bb99070440feb4d60e31f45b48e6aee
Retr0327 <philcoke35@gmail.com>
2022-10-14 00:14:01 +0800

    Wrap with try-catch block

--- src/api/controllers/url/create.ts
+++ src/api/controllers/url/create.ts
@@ -8,30 +8,35 @@ import getExpireTime from '@utils/expire';
 type Request = Pick<URL, 'url' | 'expireDate'>;
 
 const handleCreateShortURL: Middleware = async (ctx) => {
-  const { url, expireDate } = ctx.request.body as Request;
-  const shortURL = makeShortURL(5);
-  const expire = new Date(expireDate);
+  try {
+    const { url, expireDate } = ctx.request.body as Request;
+    const shortURL = makeShortURL(5);
+    const expire = new Date(expireDate);
 
-  const urlResult = await prisma.shortUrls.create({
-    data: { fullURL: url, shortURL, expire },
-    select: { id: true, fullURL: true, shortURL: true },
-  });
+    const urlResult = await prisma.shortUrls.create({
+      data: { fullURL: url, shortURL, expire },
+      select: { id: true, fullURL: true, shortURL: true },
+    });
 
-  if (urlResult === null) {
+    if (urlResult === null) {
+      ctx.status = 500;
+      ctx.body = { status: 'failed', msg: 'internal server error' };
+      return;
+    }
+
+    await hsetURL(
+      urlResult.id.toString(),
+      urlResult.fullURL,
+      urlResult.shortURL,
+      getExpireTime(expire),
+    );
+
+    ctx.status = 201;
+    ctx.body = { status: 'success', data: { id: urlResult.id, shortURL: urlResult.shortURL } };
+  } catch (error) {
     ctx.status = 500;
     ctx.body = { status: 'failed', msg: 'internal server error' };
-    return;
   }
-
-  await hsetURL(
-    urlResult.id.toString(),
-    urlResult.fullURL,
-    urlResult.shortURL,
-    getExpireTime(expire),
-  );
-
-  ctx.status = 201;
-  ctx.body = { status: 'success', data: { id: urlResult.id, shortURL: urlResult.shortURL } };
 };
 
 export default handleCreateShortURL;
