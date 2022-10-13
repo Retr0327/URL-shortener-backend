940f40bb6be2aba14984f05e7200da4413c871f7
Retr0327 <philcoke35@gmail.com>
2022-10-14 00:22:51 +0800

    Update server import path

--- test/e2e/root.test.ts
+++ test/e2e/root.test.ts
@@ -1,14 +1,14 @@
-import request from "supertest";
-import server from "src/server";
+import request from 'supertest';
+import server from 'src/index';
 
 afterAll(() => {
   server.close();
 });
 
-describe("Test root /", () => {
-  test("should return success and ip", async () => {
-    const response = await request(server).get("/");
-    const actual = { status: "success", ip: "127.0.0.1" };
+describe('Test root /', () => {
+  test('should return success and ip', async () => {
+    const response = await request(server).get('/');
+    const actual = { status: 'success', ip: '127.0.0.1' };
 
     expect(response.statusCode).toEqual(200);
     expect(response.body).toMatchObject(actual);
