3d46f2df1894a843fed5f421e686f8b37e2eef50
Retr0327 <philcoke35@gmail.com>
2022-10-14 00:46:47 +0800

    Update path in moduleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -1,30 +1,30 @@
-import type { Config } from "@jest/types";
+import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   verbose: true,
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts"],
+  collectCoverageFrom: ['src/**/*.ts'],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  moduleFileExtensions: ["ts", "js"],
+  moduleFileExtensions: ['ts', 'js'],
   moduleNameMapper: {
-    "src/(.*)": "<rootDir>/src/$1",
-    "@config": "<rootDir>/src/api/config/index.ts",
-    "@validations": "<rootDir>/src/api/validations/index.ts",
-    "@middlewares/(.*)": "<rootDir>/src/api/middlewares/$1",
-    "@utils": "<rootDir>/src/api/utils/index.ts",
-    "@controllers": "<rootDir>/src/api/controllers/index.ts",
-    "@routes": "<rootDir>/src/api/routes/index.ts",
-    "@models": "<rootDir>/src/api/models/index.ts",
-    "@types": "<rootDir>/src/api/types/index.ts",
+    'src/(.*)': '<rootDir>/src/$1',
+    '@config': '<rootDir>/src/api/config/index.ts',
+    '@validations/(.*)': '<rootDir>/src/api/validations/$1',
+    '@middlewares/(.*)': '<rootDir>/src/api/middlewares/$1',
+    '@utils/(.*)': '<rootDir>/src/api/utils/$1',
+    '@controllers/(.*)': '<rootDir>/src/api/controllers/$1',
+    '@routes': '<rootDir>/src/api/routes/index.ts',
+    '@models': '<rootDir>/src/api/models/index.ts',
+    '@types': '<rootDir>/src/api/types/index.ts',
   },
   testTimeout: 5000,
-  setupFiles: ["<rootDir>/test/__mocks__/setEnvVars.js"],
-  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(js?|ts?)$",
+  setupFiles: ['<rootDir>/test/__mocks__/setEnvVars.js'],
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(js?|ts?)$',
 };
 
 export default config;
