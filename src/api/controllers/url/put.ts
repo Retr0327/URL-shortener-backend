daeab3ee1eae25e10e598e7f8c39d29f1e6499d2
Retr0327 <philcoke35@gmail.com>
2022-10-14 22:23:56 +0800

    Set response status to 200

--- src/api/controllers/url/put.ts
+++ src/api/controllers/url/put.ts
@@ -5,22 +5,27 @@ import { prisma } from '@models';
 type Request = Pick<URL, 'shortURL'>;
 
 const handleIncreaseClick: Middleware = async (ctx) => {
-  const { shortURL } = ctx.request.body as Request;
+  try {
+    const { shortURL } = ctx.request.body as Request;
 
-  const result = await prisma.shortUrls.update({
-    where: { shortURL },
-    data: { totalClick: { increment: 1 } },
-    select: { fullURL: true },
-  });
+    const result = await prisma.shortUrls.update({
+      where: { shortURL },
+      data: { totalClick: { increment: 1 } },
+      select: { fullURL: true },
+    });
 
-  if (result === null) {
+    if (result === null) {
+      ctx.status = 500;
+      ctx.body = { status: 'failed', msg: 'internal server error' };
+      return;
+    }
+
+    ctx.status = 200;
+    ctx.body = { status: 'success', data: { url: result.fullURL } };
+  } catch (error) {
     ctx.status = 500;
     ctx.body = { status: 'failed', msg: 'internal server error' };
-    return;
   }
-
-  ctx.status = 201;
-  ctx.body = { status: 'success', data: { url: result.fullURL } };
 };
 
 export default handleIncreaseClick;
