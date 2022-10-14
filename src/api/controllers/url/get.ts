0971c352441d14eb8b7262530b706cda9294f925
Retr0327 <philcoke35@gmail.com>
2022-10-14 22:10:05 +0800

    Wrap with try-catch statement

--- src/api/controllers/url/get.ts
+++ src/api/controllers/url/get.ts
@@ -2,16 +2,21 @@ import { Middleware } from 'koa';
 import { prisma } from '@models';
 
 const handleGetAllShortURLs: Middleware = async (ctx) => {
-  const data = await prisma.shortUrls.findMany();
+  try {
+    const data = await prisma.shortUrls.findMany();
 
-  if (data === null) {
+    if (data === null) {
+      ctx.status = 500;
+      ctx.body = { status: 'failed', msg: 'internal server error' };
+      return;
+    }
+
+    ctx.status = 200;
+    ctx.body = { status: 'success', data };
+  } catch (error) {
     ctx.status = 500;
     ctx.body = { status: 'failed', msg: 'internal server error' };
-    return;
   }
-
-  ctx.status = 200;
-  ctx.body = { status: 'success', data };
 };
 
 export default handleGetAllShortURLs;
